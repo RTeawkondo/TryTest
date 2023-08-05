96accf9300b69b0057beab3d5087a0bd067137ba
Peto Gurin <no.mi83@yahoo.com.vn>
2023-08-05 20:34:10 +0700

    dung JEST de test???

--- src/components/Async.js
+++ src/components/Async.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 export default function Async() {
     const [posts,setPost] = useState([])
-    useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res)=> res.json())
-        .then((data) => {
-            setPost(data)
-        })
-    }, [])
+    // useEffect(()=>{
+    //     fetch("https://jsonplaceholder.typicode.com/posts")
+    //     .then((res)=> res.json())
+    //     .then((data) => {
+    //         setPost(data)
+    //     })
+    // }, [])
   return (
     <div>
         <ul>
--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,11 +1,16 @@
-import { render, screen } from "@testing-library/react"
-import Async from "./Async"
+import { render, screen } from "@testing-library/react";
+import { expect, jest, test } from "@jest/globals";
+import Async from "./Async";
 
-describe("Async component",()=>{
-    test("hehe",  async ()=>{
-        render(<Async/>)
+describe("Async component", () => {
+  test("hehe", async () => {
+    render(<Async />);
+    window.fetch = jest.fn();
+    await window.fetch.mockResolveValueOnce({
+      json: async () => [{ id: "p1", title: "hehe" }],
+    });
 
-        const listItem = await screen.findAllByRole("listitem")
-        expect(listItem).not.toHaveLength(0)
-    })
-})
+    const listItem = await screen.findAllByRole("listitem");
+    expect(listItem).not.toHaveLength(0);
+  });
+});
